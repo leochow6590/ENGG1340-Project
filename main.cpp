5eb4e156b984e1fba03cb424a2edf3cb08f7a060
Leo <47248018+leochow6590@users.noreply.github.com>
2019-04-25 21:14:47 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-include <iostream>
-include <string>
+#include<iostream>
+#include<string>
+#include<fstream>
 using namespace std;
 int main(){
   string username, u, password, cpassword, p;
-  if(username==NULL){
-    cout<<"Please sign up before using the system"
+  ifstream fin1;
+  fin1.open("acc.txt");
+  if(fin1.fail()){
+    cout<<"Please sign up before using the system"<<endl;
     cout<<"Create your username: ";
     getline(cin, username);
     cout<<"Create your password: ";
@@ -12,25 +15,33 @@ int main(){
     cout<<"Confirm your password: ";
     getline(cin, cpassword);
     while(password!=cpassword){
-      cout<<"Please confirm your password by re-entering it";
+      cout<<"Please confirm your password by re-entering it"<<endl;
       cout<<"Create your password: ";
       getline(cin, password);
       cout<<"Confirm your password: ";
       getline(cin, cpassword);
     }
+    ofstream fout1;
+    fout1.open("acc.txt");
+    fout1<<username<<endl;
+    fout1<<password<<endl;
+    fout1.close();
   }
   else{
     cout<<"Username: ";
     getline(cin, u);
     cout<<"Password: ";
     getline(cin, p);
+    getline(fin1, username);
+    getline(fin1, password);
+    fin1.close();
     while(u!=username or p!=password){
-      cout<<"Invalid username or password!";
+      cout<<"Invalid username or password!"<<endl;
       cout<<"Username: ";
       getline(cin, u);
       cout<<"Password: ";
       getline(cin, p);
     }
-    cout<<"Welcome back, ",username<<endl;
+    cout<<"Welcome back, "<<username<<endl;
   }
 }

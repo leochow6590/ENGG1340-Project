fa57270301e48482217089d2ba5d02ed702742b2
Leo <47248018+leochow6590@users.noreply.github.com>
2019-04-28 17:17:04 +0800

    Added function comments

--- signin.cpp
+++ signin.cpp
@@ -5,6 +5,14 @@
 #include"signin.h"
 using namespace std;
 
+//This file contains a function that allows user to sign up or sign in.//
+//Whenever user access to the system, this function will be executed.//
+//If it is the user's first access, user will be required to input a user name and a password, then confirm the password again.//
+//Above inputted information will be stored in "acc.txt"//
+//After the first access, user are required to sign in with the username and password that already inputted in the first access.//
+//"acc.txt" will be read and the input from user will be compared with the content of "acc.txt".//
+//User has to enter correct username and password in oder to sign in.//
+
 int signin(){
   ifstream fin;
   ofstream fout;
